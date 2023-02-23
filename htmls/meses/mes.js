1f2803e19fe4a2771f0a1f9fab8aa8cb01f2545a
pscolme <78502330+pscolme@users.noreply.github.com>
2023-02-23 21:32:52 +0100

    Delete htmls directory

--- htmls/meses/mes.js
+++
@@ -1,9 +0,0 @@
-// Guardando una variable en el almacenamiento local
-let data = document.getElementsByClassName("day");
-
-Array.prototype.forEach.call(data, function(item) {
-  item.addEventListener("click", function() {
-      console.log('Elemento clickado')
-      localStorage.setItem("currentDate", this.innerText);
-    });
-});
